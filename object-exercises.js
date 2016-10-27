101afe30bb223fc96b198d642ca32be3bd6f602d
Elijah <elieel@gmail.com>
2016-10-27 09:56:51 -0400

    Complete object exercises. Add histogram.js file.

--- object-exercises.js
+++ object-exercises.js
@@ -1,7 +1,7 @@
 var phonebookDict = {
   Alice: '703-493-1834',
-  Bob: '857-384-1234',
-  Elizabeth: '484-584-2923'
+  Bob: ['857-384-1234','404-444-4567','404-444-4567','404-444-4567'],
+  Elizabeth: ['484-584-2923','404-444-4567','404-444-4567','404-444-4567']
 };
 
 // Print Elizabeth's Phone Number
@@ -11,3 +11,8 @@ var phonebookDict = {
 // Add Kareem's Name and Phone Number
 // phonebookDict.Kareem = '938-489-1234';
 // console.log(phonebookDict);
+
+// Delete Alice's Phone entry
+delete phonebookDict.Bob.splice(3,2);
+delete phonebookDict.Alice[1];
+console.log(phonebookDict);
