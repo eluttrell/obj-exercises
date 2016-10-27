a6d371b34f208058ae98bbf0582591df278c8815
Elijah <elieel@gmail.com>
2016-10-27 09:32:29 -0400

    Complete Print Elizabeth's phone number and Add Entry Kareem exercises.

---
+++ object-exercises.js
@@ -0,0 +1,13 @@
+var phonebookDict = {
+  Alice: '703-493-1834',
+  Bob: '857-384-1234',
+  Elizabeth: '484-584-2923'
+};
+
+// Print Elizabeth's Phone Number
+// var phone = phonebookDict.Elizabeth;
+// console.log("Elizabeth's Phone Number:\n" + phone);
+
+// Add Kareem's Name and Phone Number
+// phonebookDict.Kareem = '938-489-1234';
+// console.log(phonebookDict);
